a8349bcbe8385e2bd1c23bb22780fadb198a83c8
urdaddygh <aziret.2000@bk.ru>
2022-05-31 22:00:21 +0600

    Created component of button, completed component of input, done registr page, install formik

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Auth from "./pages/auth/Auth";
-
+import { Registr } from "./pages/register/Registr";
+import "./App.scss"
 function App() {
   return (
     <div className="App">
-      <Auth />
+      {/* <Auth /> */}
+      <Registr />
     </div>
   );
 }
---
+++ src/images/index.js
@@ -0,0 +1,7 @@
+import open_eye from "./open_eye.svg"
+import close_eye from "./close_eye.svg"
+
+export {
+    open_eye,
+    close_eye
+}
