3fd0b094c0ba6f90240f318d70fba9b2a4d14c89
urdaddygh <aziret.2000@bk.ru>
2022-06-01 19:23:27 +0600

    set up the gradient

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -6,13 +6,16 @@ const Input = ({ placeholder, value, valueLabel, onChange, type, name }) => {
   return (
     <div className={s.input_container}>
       <label className={s.label}>{valueLabel}</label>
-      <input className={s.input} 
-      placeholder={placeholder} 
-      value={value} 
-      onChange={onChange} 
-      name={name}
-      type={type} 
-      />
+      <div className={s.gradient}>
+        <input
+          className={s.input}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          name={name}
+          type={type}
+        />
+      </div>
     </div>
   );
 };
