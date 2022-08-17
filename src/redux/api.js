f2df28cc60f2d92d3c52fa8b4f209173c56ee34a
santiill <szebiniso0909@gmail.com>
2022-08-17 21:13:55 +0600

    fixed

--- src/redux/api.js
+++ src/redux/api.js
@@ -28,82 +28,82 @@ const notToken = axios.create({
 
 export const requests = {
     authApi: (data) => fetchHerokuAPI.post("login/", data),
-    resetPasswordApi: (data) => fetchAPI.post("password-reset/", data),
-    feedbackApi: (data) => fetchAPI.post("feedback/", data),
-    newPasswordApi: (data) => fetchAPI.post("password-reset/confirm/", data),
-    getFeedback: () => fetchAPI.get("feedback/"),
-    getFeedbackById: (id) => fetchAPI.get(`feedback/${id}`),
+    resetPasswordApi: (data) => fetchHerokuAPI.post("password-reset/", data),
+    feedbackApi: (data) => fetchHerokuAPI.post("feedback/", data),
+    newPasswordApi: (data) => fetchHerokuAPI.post("password-reset/confirm/", data),
+    getFeedback: () => fetchHerokuAPI.get("feedback/"),
+    getFeedbackById: (id) => fetchHerokuAPI.get(`feedback/${id}`),
 
     // Events
     getEvents:() => fetchHerokuAPI.get('event/'),
-    postEvents:(data) => fetchAPI.post('event/', data),
-    editEvents:(data) => fetchAPI.patch(`event/${data.id}/`, data.values),
-    deleteEvents:(id) => fetchAPI.delete(`event/${id}/`),
+    postEvents:(data) => fetchHerokuAPI.post('event/', data),
+    editEvents:(data) => fetchHerokuAPI.patch(`event/${data.id}/`, data.values),
+    deleteEvents:(id) => fetchHerokuAPI.delete(`event/${id}/`),
 
     //User
-    getJudgeUser:() => fetchAPI.get('user/?is_judge=true'),
-    getAllUser:() => fetchAPI.get('user/'),
-    getNotRegisterUser:() => fetchAPI.get('user/'),
-    getUserById:(id) => fetchAPI.get(`user/${id}`),
-    getSecretaryUser:() => fetchAPI.get('user/?is_assistant=true'),
-    getUserForProfile:(id) => fetchAPI.get(`user/${id}`),
-    deleteUser:(id) => fetchAPI.delete(`user/${id}`),
-    editUser:(data) => fetchAPI.patch(`user/${data.id}/`, data.values),
-    postUserClub:(data) => fetchAPI.post(`user_club/`, data),
-    getTrainerUser: () => fetchAPI.get("user/", { params: { is_assistant: false, is_judge : false, role: "TRAINER" } }),
+    getJudgeUser:() => fetchHerokuAPI.get('user/?is_judge=true'),
+    getAllUser:() => fetchHerokuAPI.get('user/'),
+    getNotRegisterUser:() => fetchHerokuAPI.get('user/'),
+    getUserById:(id) => fetchHerokuAPI.get(`user/${id}`),
+    getSecretaryUser:() => fetchHerokuAPI.get('user/?is_assistant=true'),
+    getUserForProfile:(id) => fetchHerokuAPI.get(`user/${id}`),
+    deleteUser:(id) => fetchHerokuAPI.delete(`user/${id}`),
+    editUser:(data) => fetchHerokuAPI.patch(`user/${data.id}/`, data.values),
+    postUserClub:(data) => fetchHerokuAPI.post(`user_club/`, data),
+    getTrainerUser: () => fetchHerokuAPI.get("user/", { params: { is_assistant: false, is_judge : false, role: "TRAINER" } }),
 
     // documentation
-    getDocs: () => fetchAPI.get("documents/document/"),
-    getDoc: (id) => fetchAPI.get(`documents/document/${id}/`),
-    postDoc: (data) => fetchAPI.post("documents/document/", data.data),
-    editDoc: (data) => fetchAPI.patch(`documents/document/${data.id}/`, data.fData),
-    deleteDoc: (id) => fetchAPI.delete(`documents/document/${id}/`),
+    getDocs: () => fetchHerokuAPI.get("documents/document/"),
+    getDoc: (id) => fetchHerokuAPI.get(`documents/document/${id}/`),
+    postDoc: (data) => fetchHerokuAPI.post("documents/document/", data.data),
+    editDoc: (data) => fetchHerokuAPI.patch(`documents/document/${data.id}/`, data.fData),
+    deleteDoc: (id) => fetchHerokuAPI.delete(`documents/document/${id}/`),
 
     // news
-    getNewsApi: () => fetchAPI.get("createnew/"),
-    getNewApi: (id) => fetchAPI.get(`deletenew/${id}`),
-    createNewsApi: (data) => fetchAPI.post("createnew/", data.data),
-    editNewsApi: (data) => fetchAPI.patch(`deletenew/${data.id}`, data.data),
-    deleteNewsApi: (id) => fetchAPI.delete(`deletenew/${id}`),
+    getNewsApi: () => fetchHerokuAPI.get("createnew/"),
+    getNewApi: (id) => fetchHerokuAPI.get(`deletenew/${id}`),
+    createNewsApi: (data) => fetchHerokuAPI.post("createnew/", data.data),
+    editNewsApi: (data) => fetchHerokuAPI.patch(`deletenew/${data.id}`, data.data),
+    deleteNewsApi: (id) => fetchHerokuAPI.delete(`deletenew/${id}`),
 
     //Applications
-    getApplication: () => fetchAPI.get("application/"),
-    getApplicationById: (id) => fetchAPI.get(`application/${id}/`),
-    deleteApplicationById: (id) => fetchAPI.delete(`application/${id}/`),
-    postApplication: (data) => fetchAPI.post(`application/`, data),
-    editApplicationById: (data) => fetchAPI.patch(`application/${data.applicationId}/`, data),
-    getCurrentApplication:(data)=>fetchAPI.get(`application/?trainer=${data.id}&new_application=2022-09-12`),
-    getHistoryApplication:(data)=>fetchAPI.get(`application/?trainer=${data.id}&old_application=2022-09-12`),
-    postApplicationTemplate: (data) => fetchAPI.post(`template_application/`, data),
-    editApplicationTemplate: (data) => fetchAPI.patch(`template_application/${data.id}`, data),
-    getApplicationTemplate:()=>fetchAPI.get("template_application/"),
-    getApplicationTemplateById:(id)=>fetchAPI.get(`template_application/${id}`),
+    getApplication: () => fetchHerokuAPI.get("application/"),
+    getApplicationById: (id) => fetchHerokuAPI.get(`application/${id}/`),
+    deleteApplicationById: (id) => fetchHerokuAPI.delete(`application/${id}/`),
+    postApplication: (data) => fetchHerokuAPI.post(`application/`, data),
+    editApplicationById: (data) => fetchHerokuAPI.patch(`application/${data.applicationId}/`, data),
+    getCurrentApplication:(data)=>fetchHerokuAPI.get(`application/?trainer=${data.id}&new_application=2022-09-12`),
+    getHistoryApplication:(data)=>fetchHerokuAPI.get(`application/?trainer=${data.id}&old_application=2022-09-12`),
+    postApplicationTemplate: (data) => fetchHerokuAPI.post(`template_application/`, data),
+    editApplicationTemplate: (data) => fetchHerokuAPI.patch(`template_application/${data.id}`, data),
+    getApplicationTemplate:()=>fetchHerokuAPI.get("template_application/"),
+    getApplicationTemplateById:(id)=>fetchHerokuAPI.get(`template_application/${id}`),
 
     //Discipline
-    getDiscipline: () => fetchAPI.get(`discipline/`),
+    getDiscipline: () => fetchHerokuAPI.get(`discipline/`),
 
     //Athletes
-    getAthletes: () => fetchAPI.get(`athletes/`),
+    getAthletes: () => fetchHerokuAPI.get(`athletes/`),
 
 
     // club
-    getClubsApi: () => fetchAPI.get("club/"),
-    getClubApi: (id) => fetchAPI.get(`club/${id}`),
-    postClubApi: (data) => fetchAPI.post("club/", data.data),
-    editClubApi: (data) => fetchAPI.patch(`club/${data.id}/`, data.data),
-    deleteClubApi: (id) => fetchAPI.delete(`club/${id}/`),
+    getClubsApi: () => fetchHerokuAPI.get("club/"),
+    getClubApi: (id) => fetchHerokuAPI.get(`club/${id}`),
+    postClubApi: (data) => fetchHerokuAPI.post("club/", data.data),
+    editClubApi: (data) => fetchHerokuAPI.patch(`club/${data.id}/`, data.data),
+    deleteClubApi: (id) => fetchHerokuAPI.delete(`club/${id}/`),
 
     // sportsmen
-    getSportsmenApi: () => fetchAPI.get("athletes/"),
-    getSportsmanApi: (id) => fetchAPI.get(`athletes/${id}`),
-    postSportsmenApi: (data) => fetchAPI.post("athletes/", data.data),
-    editSportsmenApi: (data) => fetchAPI.patch(`athletes/${data.id}/`, data.data),
-    deleteSportsmenApi: (id) => fetchAPI.delete(`athletes/${id}/`),
+    getSportsmenApi: () => fetchHerokuAPI.get("athletes/"),
+    getSportsmanApi: (id) => fetchHerokuAPI.get(`athletes/${id}`),
+    postSportsmenApi: (data) => fetchHerokuAPI.post("athletes/", data.data),
+    editSportsmenApi: (data) => fetchHerokuAPI.patch(`athletes/${data.id}/`, data.data),
+    deleteSportsmenApi: (id) => fetchHerokuAPI.delete(`athletes/${id}/`),
 
     // Physical Indicator
     // getSportsmanApi: (id) => fetchAPI.get(`athletes/${id}`),
-    postPhysicalIndicatorApi: (data) => fetchAPI.post("physical_indicators/", data),
-    editPhysicalIndicatorApi: ({id, data}) => fetchAPI.patch(`physical_indicators/${id}/`, data),
+    postPhysicalIndicatorApi: (data) => fetchHerokuAPI.post("physical_indicators/", data),
+    editPhysicalIndicatorApi: ({id, data}) => fetchHerokuAPI.patch(`physical_indicators/${id}/`, data),
     // deleteSportsmenApi: (id) => fetchAPI.delete(`athletes/${id}/`),
 
     // protocol
@@ -119,8 +119,8 @@ export const requests = {
     getJudgeApi: () => fetchHerokuAPI.get("judge_group/"),
     // getProtocolApi: (id) => fetchAPI.get(`judge_group/${id}`),
     postJudgeApi: (data) => fetchHerokuAPI.post("judge_group/", data),
-    formJudgeApi: (data) => fetchAPI.patch(`judge_group/${data.id}/`, {areas_quantity: +data.data.areas_quantity, top_places_percent: data.data.top_places_percent}),
-    confirmJudgeApi: ({id, par}) => fetchAPI.patch(`judge_group/${id}/`, {is_confirmed : par}),
+    formJudgeApi: (data) => fetchHerokuAPI.patch(`judge_group/${data.id}/`, {areas_quantity: +data.data.areas_quantity, top_places_percent: data.data.top_places_percent}),
+    confirmJudgeApi: ({id, par}) => fetchHerokuAPI.patch(`judge_group/${id}/`, {is_confirmed : par}),
 }
 
 export const withoutToken = {
